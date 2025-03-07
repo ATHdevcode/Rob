b742e2808720af799762db63e9d775c463c4a031
Aswanth.k <aswanthsuttu666@gmail.com>
2025-03-07 22:01:12 +0530

    smokesandspeed

--- src/key_door.gd
+++ src/key_door.gd
@@ -5,5 +5,15 @@ extends StaticBody2D
 
 func _on_pin_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and body.has_key:
-		queue_free()
+		$AnimationPlayer.play("key")
 		
+		
+		
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	$AnimatedSprite2D.play("default")
--- src/player.gd
+++ src/player.gd
@@ -3,14 +3,30 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -550.0
+var booster = 1;
 
 var is_hiddenin = false
+var is_hid = false
 var hidable = false
 var hidearea;
 
 var has_key = false;
 
+enum nintech {
+	SMOKE, SPEED
+}
+var moves = [nintech.SMOKE, nintech.SPEED]
 
+@onready var smokes = load("res://entity/smoke.tscn")
+
+
+
+	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("a"):
+		usemove(moves[0])
+	if Input.is_action_just_pressed("b"):
+		usemove(moves[1])
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,32 +39,42 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		
 	if not is_on_floor():
-		$AnimatedSprite2D.play("jump")
+		pass
+		#$AnimatedSprite2D.play("jump")
 		
 		if is_hiddenin:
-			$ColorRect.z_index = 0;
+			$AnimatedSprite2D.z_index = 0;
 			is_hiddenin = false
 			hidable = true
 
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
+	$AnimationTree.set("parameters/conditions/ideal", !direction)
+	$AnimationTree.set("parameters/conditions/run", direction)
+	
+	
 	if not is_hiddenin:
 		if direction:
 			$AnimatedSprite2D.scale.x = direction;
-			$AnimatedSprite2D.play("run")
-			velocity.x = direction * SPEED
+			
+			
+			velocity.x = direction * SPEED*booster
 		else:
 			if is_on_floor():
-				$AnimatedSprite2D.play("ideal")
+				pass
+				
+				
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+			
 			
 	
 			
 	if hidable and Input.is_action_just_pressed("click"):
 		playerhide(position, hidearea)
 	elif is_hiddenin and Input.is_action_just_pressed("click"):
-		$ColorRect.z_index = 0;
+		$AnimatedSprite2D.z_index = 0;
 		is_hiddenin = false
 		hidable = true
 		
@@ -61,7 +87,7 @@ func playerhide(position, area):
 	is_hiddenin = true
 	hidable = false
 	position.x = area.position.x;
-	$ColorRect.z_index -= 1
+	$AnimatedSprite2D.z_index -= 1
 
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
@@ -77,11 +103,11 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 		area.queue_free()
 	
 	#guards
-	if area.name == "point" and not is_hiddenin:
+	if area.name == "point" and not is_hiddenin and not is_hid:
 		print("GOT CAUGHT!!!!!$%%%")
 		get_tree().reload_current_scene();
 	#cctv
-	if area.name == "RayCast2D" and not is_hiddenin:
+	if area.name == "RayCast2D" and not is_hiddenin and not is_hid:
 		get_tree().reload_current_scene();
 	
 	if area.is_in_group("hide"):
@@ -93,3 +119,35 @@ func _on_hitbox_area_exited(area: Area2D) -> void:
 	if area.is_in_group("hide"):
 		hidable = false
 		is_hiddenin = false
+		
+func usemove(move):
+	if move == nintech.SMOKE:
+		$AnimationTree.set("parameters/conditions/smoke", true)
+		
+		var inst = smokes.instantiate()
+		inst.position.x = position.x
+		inst.position.y = position.y+20
+		get_parent().add_child(inst)
+	elif move == nintech.SPEED:
+		booster = 2
+		$speed.start()
+		$AnimatedSprite2D/CPUParticles2D.emitting = true
+		$AnimatedSprite2D/CPUParticles2D2.emitting = true
+		$AnimatedSprite2D/CPUParticles2D3.emitting = true
+
+
+func _on_speed_timeout() -> void:
+	booster = 1
+	$AnimatedSprite2D/CPUParticles2D.emitting = false
+	$AnimatedSprite2D/CPUParticles2D2.emitting = false
+	$AnimatedSprite2D/CPUParticles2D3.emitting = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "smoke":
+		print("done")
+		$AnimationTree.set("parameters/conditions/smoke", false)
+	
+func smokefin():
+	$AnimationTree.set("parameters/conditions/smoke", false)
+	
